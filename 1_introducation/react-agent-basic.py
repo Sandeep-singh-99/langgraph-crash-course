9e044760f0cd1b88826ba6779ce262ba8154f19e
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-30 11:31:07 +0530

    Refactor agent initialization and search tool integration in react-agent-basic.py

--- 1_introducation/react-agent-basic.py
+++ 1_introducation/react-agent-basic.py
@@ -1,10 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain.agents import initialize_agent
+from langchain_community.tools import TavilySearchResults
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.0);
 
-result = llm.invoke("What is the capital of France?")
+search_tool = TavilySearchResults(search_depth="basic")
 
-print(result)
+tools = [search_tool]
+
+agents = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True)
+
+agents.invoke("how to find and apply remote internships in Full Stack Development?")
