d9bf924c9ffc9b49e26bc09ae8517ede43e674f6
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-08-08 18:09:04 +0530

    langgraph implement memory

--- basic_chabot/main.py
+++ basic_chabot/main.py
@@ -8,7 +8,7 @@ from langgraph.graph.message import add_messages
 from langchain_community.tools import TavilySearchResults
 from langchain.agents import initialize_agent
 from langchain_core.messages import AIMessage
-# from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
 
 
 # Load environment variables
@@ -25,7 +25,7 @@ search_tool = TavilySearchResults(search_depth="basic")
 
 tools = [search_tool]
 
-agents = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True)
+agents = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True, handle_parsing_errors=True)
 
 # Define the chatbot node
 def chatbot(state: State) -> State:
@@ -41,12 +41,34 @@ graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
-# memory = InMemorySaver()
+memory = InMemorySaver()
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory)
+
+thread_id = 'user_thread_1'
 
 # Run the graph with an initial message
-result = graph.invoke({"messages": [{"role": "user", "content": "How to find and apply remote internships in Full Stack Development?"}]})
+result = graph.invoke(
+    {"messages": [{"role": "user", "content": "How to find and apply remote internships in Full Stack Development?"}]},
+    config={"configurable": {"thread_id": thread_id}}
+)
 
 # Print response
 print(result["messages"][-1].content)
+
+
+# Run another query in the same thread to demonstrate memory
+
+result = graph.invoke(
+    {"messages": [{"role": "user", "content": "Can you give more details about the skills needed?"}]},
+    config={"configurable": {"thread_id": thread_id}}
+)
+
+
+# Print the new response
+print("\n Follow up response.")
+print(result["messages"][-1].content)
+
+
+
+
